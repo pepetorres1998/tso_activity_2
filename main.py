fff35018dabcef2454a844767c3447138ae7b681
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2019-08-27 16:13:23 -0500

    Add result printing

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from element import Element
+from package import Package
+import pdb
 
 if(__name__=='__main__'):
     with open('values.txt', 'r') as f:
         values = [Element(line.rstrip()) for line in f.readlines()]
-        print([value.id for value in values])
+        p = Package(30)
+        values.sort(key=lambda x: x.relation, reverse=True)
 
+        for value in values:
+            p.add_element(value)
+
+        print('Elementos en la mochila:', '\n')
+        print(p.elements, '\n')
+        print('Valor total de la mochila: {total}'.format(total=p.value))
+        print('Peso total de la mochila: {weight}'.format(weight=p.weight))
